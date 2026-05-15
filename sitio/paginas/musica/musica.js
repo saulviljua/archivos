c6ba22be4a7713b30c0916c30b15296352f9aeb5
Saul <jacktrotamundos95@gmail.com>
2026-05-15 15:10:42 -0400

    Add files via upload

---
+++ sitio/paginas/musica/musica.js
@@ -0,0 +1,112 @@
+function showImagePetalos(container) {
+	const encima_petalos = document.getElementById("encima_petalos");
+	const encima_petalos_img = document.getElementById("encima_petalos_img");
+	const audiopetalos = document.getElementById("petalos");
+	const imgSrc = document.getElementById("encima_petalos_img").src;
+	encima_petalos_img.src = imgSrc;
+	encima_petalos.style.display = "flex";
+	audiopetalos.currentTime = 0;
+	audiopetalos.play();
+}
+function hideImagePetalos() {
+	const encima_petalos = document.getElementById("encima_petalos");
+	const audiopetalos = document.getElementById("petalos");
+	encima_petalos.style.display = "none";
+	audiopetalos.pause();
+}
+function showImageRetratos(container) {
+	const encima_retratos = document.getElementById("encima_retratos");
+	const encima_retratos_img = document.getElementById("encima_retratos_img");
+	const audioretratos = document.getElementById("retratos");
+	const imgSrc = document.getElementById("encima_retratos_img").src;
+	encima_retratos_img.src = imgSrc;
+	encima_retratos.style.display = "flex";
+	audioretratos.currentTime = 0;
+	audioretratos.play();
+}
+function hideImageRetratos() {
+	const encima_retratos = document.getElementById("encima_retratos");
+	const audioretratos = document.getElementById("retratos");
+	encima_retratos.style.display = "none";
+	audioretratos.pause();
+}
+function showImageIntencion(container) {
+	const encima_intencion = document.getElementById("encima_intencion");
+	const encima_intencion_img = document.getElementById("encima_intencion_img");
+	const audiointencion = document.getElementById("intencion");
+	const imgSrc = document.getElementById("encima_intencion_img").src;
+	encima_intencion_img.src = imgSrc;
+	encima_intencion.style.display = "flex";
+	audiointencion.currentTime = 0;
+	audiointencion.play();
+}
+function hideImageIntencion() {
+	const encima_intencion = document.getElementById("encima_intencion");
+	const audiointencion = document.getElementById("intencion");
+	encima_intencion.style.display = "none";
+	audiointencion.pause();
+}
+function showImageExtrano(container) {
+	const encima_extrano = document.getElementById("encima_extrano");
+	const encima_extrano_img = document.getElementById("encima_extrano_img");
+	const audioextrano = document.getElementById("extrano");
+	const imgSrc = document.getElementById("encima_extrano_img").src;
+	encima_extrano_img.src = imgSrc;
+	encima_extrano.style.display = "flex";
+	audioextrano.currentTime = 0;
+	audioextrano.play();
+}
+function hideImageExtrano() {
+	const encima_extrano = document.getElementById("encima_extrano");
+	const audioextrano = document.getElementById("extrano");
+	encima_extrano.style.display = "none";
+	audioextrano.pause();
+}
+function showImageCiguena(container) {
+	const encima_ciguena = document.getElementById("encima_ciguena");
+	const encima_ciguena_img = document.getElementById("encima_ciguena_img");
+	const audiociguena = document.getElementById("ciguena");
+	const imgSrc = document.getElementById("encima_ciguena_img").src;
+	encima_ciguena_img.src = imgSrc;
+	encima_ciguena.style.display = "flex";
+	audiociguena.currentTime = 0;
+	audiociguena.play();
+}
+function hideImageCiguena() {
+	const encima_ciguena = document.getElementById("encima_ciguena");
+	const audiociguena = document.getElementById("ciguena");
+	encima_ciguena.style.display = "none";
+	audiociguena.pause();
+}
+function showImageSolubilidad(container) {
+	const encima_solubilidad = document.getElementById("encima_solubilidad");
+	const encima_solubilidad_img = document.getElementById("encima_solubilidad_img");
+	const audiosolubilidad = document.getElementById("solubilidad");
+	const imgSrc = document.getElementById("encima_solubilidad_img").src;
+	encima_solubilidad_img.src = imgSrc;
+	encima_solubilidad.style.display = "flex";
+	audiosolubilidad.currentTime = 0;
+	audiosolubilidad.play();
+}
+function hideImageSolubilidad() {
+	const encima_solubilidad = document.getElementById("encima_solubilidad");
+	const audiosolubilidad = document.getElementById("solubilidad");
+	encima_solubilidad.style.display = "none";
+	audiosolubilidad.pause();
+}
+function showImageNosotros(container) {
+	const encima_nosotros = document.getElementById("encima_nosotros");
+	const encima_nosotros_img = document.getElementById("encima_nosotros_img");
+	const audionosotros = document.getElementById("nosotros");
+	const imgSrc = document.getElementById("encima_nosotros_img").src;
+	encima_nosotros_img.src = imgSrc;
+	encima_nosotros.style.display = "flex";
+	audionosotros.currentTime = 0;
+	audionosotros.play();
+}
+function hideImageNosotros() {
+	const encima_nosotros = document.getElementById("encima_nosotros");
+	const audionosotros = document.getElementById("nosotros");
+	encima_nosotros.style.display = "none";
+	audionosotros.pause();
+}
